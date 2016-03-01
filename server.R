a954d8a79003b804d9a62331b9bc11337585917c
Stefan Jansson <janzzon@gmail.com>
2016-03-01 20:50:41 +0100

    Change pin_ctrl2() and oin_ctrl2() to pin_ctrl( , force_logical = TRUE) and oin_ctrl( , force_logical = TRUE)

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
 library(shiny)
 library(sweidnumbr)
 library(dplyr)
-if(!("devtools" %in% installed.packages())) install.packages("devtools")
-if(!try(require(sweidnumbrExtras))){
-  devtools::install_github("janzzon/sweidnumbrExtras")
-  library(sweidnumbrExtras)
-}
+# if(!("devtools" %in% installed.packages())) install.packages("devtools")
+# if(!try(require(sweidnumbrExtras))){
+#   devtools::install_github("janzzon/sweidnumbrExtras")
+#   library(sweidnumbrExtras)
+# }
 
 server <- function(input, output) {
   # Trim input string from leadin and trailing white space, assign to reactive function
@@ -17,12 +17,12 @@ server <- function(input, output) {
       
     })
   output$result2 <- renderText({
-    if (oin_ctrl2(input_inmat_pin())){
+    if (oin_ctrl(input_inmat_pin(), force_logical = TRUE)){
       # Check if number is an organization number
       paste0("This is a swedish organization identiy number for an organization of type \"", oin_group(input_inmat_pin()), "\" ")
     } else {
       
-      if (pin_ctrl2(input_inmat_pin())){
+      if (pin_ctrl(input_inmat_pin(), force_logical = TRUE)){
         # Check if number is a personal number and extract sex and age  
         paste0("This is a swedish personal number of a ",pin_age(input_inmat_pin())," year old " 
                ,tolower(pin_sex(input_inmat_pin())), 
@@ -33,7 +33,7 @@ server <- function(input, output) {
                ,"."
         )
       } else {
-        if (!(pin_ctrl2(input_inmat_pin()) && !oin_ctrl2(input_inmat_pin()))){
+        if (!(pin_ctrl(input_inmat_pin(), force_logical = TRUE) && !oin_ctrl(input_inmat_pin(), force_logical = TRUE))){
           # Show this text if no valid personal|organizatin number.
           paste0("This is not a valid swedish personal or organization number. 
       Input a valid number or refresh page to get a random personal number")
