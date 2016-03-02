d1fb5bc7a9cf8e8ae5e736d9ed42c0fb21d99dfe
Stefan Jansson <janzzon@gmail.com>
2016-03-02 15:00:47 +0100

    Remove unused #:ed code

--- ui.R
+++ ui.R
@@ -2,15 +2,6 @@ library(shiny)
 library(sweidnumbr)
 library(dplyr)
 
-# Wrapper function for sweidnumbr::pin_ctrl to set failing pin input to FALSE and valid to TRUE
-# pin_ctrl2 <- Vectorize({
-#   function(x) {
-#     ifelse (!is.na(try(suppressWarnings(suppressMessages(pin_ctrl(x))),silent = TRUE)) &
-#               (class(try(suppressWarnings(pin_ctrl(x)), silent =TRUE)) != "try-error")  ,
-#             pin_ctrl(x), FALSE)
-#   }
-# })
-
 # Generate a random but valid pin no from data set sweidnumbr::fake_pins
 rnd_fake_pin <- function() {
   fake_pins_valid <- sweidnumbr::fake_pins$pin[pin_ctrl(fake_pins$pin, force_logical = TRUE)]
@@ -27,6 +18,4 @@ ui <- fluidPage(
   h5("Result:"),
   textOutput("result1"),
   textOutput("result2")
-  #   p("Mer text"),
-  #   p("en till rad")
   )
